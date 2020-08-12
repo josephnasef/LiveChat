5462a9a593d22cdd931fc8608017d5edbcb119bf
joseph <55623248+josephnasef@users.noreply.github.com>
2020-08-12 16:13:50 +0200

    Add project files.

---
+++ WebApplication8/Hub/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace WebApplication2
+{
+    [HubName("ChatHub")]
+    //THIS IS HUB
+    public class ChatHub : Hub
+    {
+
+        public void sendmessage(string name, string message)
+        {
+            //function : pushing content (Name , Message )
+            Clients.All.PushMessage(name, message);
+        }
+    }
+}
---
+++ WebApplication8/Scripts/chat.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+	////connection to class called chathub
+	var chat = $.connection.ChatHub;
+
+	////recive message from users 
+	chat.client.PushMessage = function (name, message) {
+		$("#discussion").append("<li> <strong> " + name + " </strong> says :   <p>" + message + "</p></li>");
+	};
+
+	////get username and save it in hidden text
+	$("#displayname").val(prompt(" please enter your name ", ''));
+
+	////get focus on textboxt of message 
+	$("#textmessage").focus();
+
+	////connectoin to hub is successfully :)
+	$.connection.hub.start().done(function () {
+		$("#sendmessage").click(function () {
+				
+				var name = $("#displayname").val();
+				var message = $("#textmessage").val();
+
+				chat.server.sendmessage(name, message);
+				$("#textmessage").val(' ').focus();
+			});
+		});
+
+});
